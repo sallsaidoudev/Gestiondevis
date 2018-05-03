32681ac2d888afb7cd053f325b5a8504241feac6
sallsaidoudev <sallsaidoudev@gmail.com>
2018-05-03 14:25:24 +0200

    GDAPI-1 Modification du fichier create.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-body">
                     <h5 class="card-title">Gestion client</h5>
                     <hr>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Enregistrer vos clients,Editer les informations et supprimer .</p>
                     <a href="/clients" class="btn btn-outline-warning">Gerer</a>
                 </div>
             </div>
